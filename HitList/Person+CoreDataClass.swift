715dfb57f25edf07457ab17af24b79aaeac4dcd8
Paweł Liczmański <paweliczek@gmail.com>
2017-04-25 22:01:49 +0200

    tworzenei NSManagedObject Subclasses

---
+++ HitList/Person+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Person+CoreDataClass.swift
+//  HitList
+//
+//  Created by Paweł Liczmański on 25.04.2017.
+//  Copyright © 2017 Razeware. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class Person: NSManagedObject {
+
+}
